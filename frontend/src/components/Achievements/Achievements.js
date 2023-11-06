6c7e99be769728415fc38b573cb43a5e8b31d1e9
rambharathmva <ram.bharath@dialpad.com>
2023-11-06 12:56:06 +0530

    front end Achievements page

---
+++ frontend/src/components/Achievements/Achievements.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import achievementsData from './achievementsData';
+
+function Achievements() {
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [selectedTeam, setSelectedTeam] = useState('');
+  const [displayedAchievements, setDisplayedAchievements] = useState(null);
+
+  // Extract unique months and teams using Set data structure
+  const uniqueMonths = [...new Set(achievementsData.map(data => data.month))];
+  const uniqueTeams = [...new Set(achievementsData.map(data => data.team))];
+
+  const handleButtonClick = () => {
+    const matchingAchievements = achievementsData.find(
+      (data) => data.month === selectedMonth && data.team === selectedTeam
+    );
+
+    setDisplayedAchievements(matchingAchievements);
+  };
+
+  return (
+    <div className="achievements-page d-ta-center">
+      <h1>Achievements</h1>
+
+      <div className="dropdowns">
+        <select
+          value={selectedMonth}
+          onChange={(e) => setSelectedMonth(e.target.value)}
+        >
+          <option value="">Select Month</option>
+          {uniqueMonths.map((month) => (
+            <option key={month} value={month}>
+              {month}
+            </option>
+          ))}
+        </select>
+
+        <select
+          value={selectedTeam}
+          onChange={(e) => setSelectedTeam(e.target.value)}
+        >
+          <option value="">Select Team</option>
+          {uniqueTeams.map((team) => (
+            <option key={team} value={team}>
+              {team}
+            </option>
+          ))}
+        </select>
+
+        <button onClick={handleButtonClick}>Get Details</button>
+      </div>
+
+      {displayedAchievements && (
+        <div className="displayed-achievements d-ta-center d-bc-magneta-200 d-ba">
+          <h2>{displayedAchievements.month} Achievements</h2>
+          <ul>
+            {displayedAchievements.achievements.map((achievement) => (
+              <li key={achievement.id}>
+                <h3>{achievement.name}</h3>
+                <p>{achievement.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Achievements;
---
+++ frontend/src/components/Achievements/achievementsData.js
@@ -0,0 +1,76 @@
+const achievementsData = [
+  {
+    month: 'January',
+    team: 'Automation',
+    achievements: [
+      {
+        id: 1,
+        name: 'Testing Novice',
+        description: 'You executed your first automated test.',
+        icon: 'novice.png',
+      },
+      {
+        id: 2,
+        name: 'Test Explorer',
+        description: "You've executed 10 automated tests.",
+        icon: 'explorer.png',
+      },
+    ],
+  },
+  {
+    month: 'January',
+    team: 'Manual',
+    achievements: [
+      {
+        id: 11,
+        name: 'Manual Testing Beginner',
+        description: 'You started your journey in manual testing.',
+        icon: 'manual-novice.png',
+      },
+      {
+        id: 12,
+        name: 'Manual Explorer',
+        description: "You've performed 20 manual test cases.",
+        icon: 'manual-explorer.png',
+      },
+    ],
+  },
+  {
+    month: 'February',
+    team: 'Automation',
+    achievements: [
+      {
+        id: 4,
+        name: 'Scripting Pro',
+        description: 'You\'ve written custom test scripts for various platforms.',
+        icon: 'scripting-pro.png',
+      },
+      {
+        id: 5,
+        name: 'Defect Detective',
+        description: 'You\'ve found and reported 5 automation-related defects.',
+        icon: 'defect-detective.png',
+      },
+    ],
+  },
+  {
+    month: 'February',
+    team: 'Manual',
+    achievements: [
+      {
+        id: 13,
+        name: 'Manual Scripter',
+        description: 'You\'ve created manual test scripts for critical scenarios.',
+        icon: 'manual-scripter.png',
+      },
+      {
+        id: 14,
+        name: 'Manual Quality Champion',
+        description: 'You\'ve ensured high-quality manual test execution.',
+        icon: 'manual-champion.png',
+      },
+    ],
+  },
+];
+
+export default achievementsData;
