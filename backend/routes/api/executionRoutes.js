5a81c1cbd16ae44060d93e5174fc014198496d9f
jatothdialpad <jatoth.rajkumar@dialpad.com>
2023-11-09 17:45:01 +0530

    execute workflow selection

--- backend/routes/api/executionRoutes.js
+++ backend/routes/api/executionRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { postExecute, getWorkflow } = require("../controllers/executionController")
 
-router.route("/execute").get(postExecute);
+router.route("/execute/:url").post(postExecute);
 
 router.route("/workflows/:id").get(getWorkflow)
 
